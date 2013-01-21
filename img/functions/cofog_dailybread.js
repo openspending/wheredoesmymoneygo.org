759b51b986d72aecc2142d08f8586265c3c3c35d
Friedrich Lindenberg <friedrich@pudo.org>
2013-01-21 19:37:13 +0100

    Fix the daily bread again.

---
+++ img/functions/cofog_dailybread.js
@@ -0,0 +1,105 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+ 
+var OpenSpending = OpenSpending || {};
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+	'01': { icon: 'img/functions/government-uk.svg', color: '#C75746' },
+	'01.1': { icon: 'img/functions/legislative.svg' },
+	'01.1.1': { icon: 'img/functions/legislative.svg' },
+	'01.1.2': { icon: 'img/functions/pig.svg' },
+	'01.1.3': { icon: 'img/functions/worldmap.svg' },
+	'01.2': { icon: 'img/functions/aid.svg' },
+	'01.2.1': { icon: 'img/functions/aid-developing-countries.svg' },
+	'01.2.2': { icon: 'img/functions/economic-aid.svg' },
+	'01.3': { icon: 'img/functions/misc-services.svg' },
+	'01.3.1': { icon: 'img/functions/human-resources.svg' },
+	'01.3.2': { icon: 'img/functions/planning.svg' },
+	'01.3.3': { icon: 'img/functions/research.svg' },
+	'01.7': { icon: 'img/functions/public-debt.svg' },
+	
+	'02': { icon: 'img/functions/defence.svg', color: '#0AB971' },
+	'02.1': { icon: 'img/functions/military.svg' },
+	'02.2': { icon: 'img/functions/civil-defence.svg' },
+	'02.3': { icon: 'img/functions/foreign-military-aid.svg' },
+	'02.4': { icon: 'img/functions/defence-research.svg' },
+	'02.5': { icon: 'img/functions/defence-admin.svg' },
+	
+	'03': { icon: 'img/functions/order-safety.svg', color: '#EC2406' }, 
+	'03.1': { icon: 'img/functions/police.svg' },
+	'03.2': { icon: 'img/functions/fire-brigade.svg' },
+	'03.3': { icon: 'img/functions/courts.svg' },
+	'03.4': { icon: 'img/functions/prisons.svg' },
+	'03.5': { icon: 'img/functions/rd-order-safety.svg' },
+	'03.6': { icon: 'img/functions/admin-order-safety.svg' },
+	
+	'04': { icon: 'img/functions/social-systems.svg', color: '#790586' }, 
+	'04.1': { icon: 'img/functions/social-systems.svg' },
+	'04.1.1': { icon: 'img/functions/social-systems.svg' },
+	'04.1.2': { icon: 'img/functions/labour.svg' },					
+	'04.2': { icon: 'img/functions/farms.svg' },
+	'04.2.1': { icon: 'img/functions/farms.svg' },
+	'04.2.2': { icon: 'img/functions/forest.svg' },
+	'04.2.3': { icon: 'img/functions/fishing.svg' },				
+	'04.3': { icon: 'img/functions/energy.svg' },
+	'04.3.1': { icon: 'img/functions/coal.svg' },
+	'04.3.2': { icon: 'img/functions/petrol.svg' },
+	'04.3.3': { icon: 'img/functions/nuclear.svg' },
+	'04.3.4': { icon: 'img/functions/fuel.svg' },
+	'04.3.5': { icon: 'img/functions/electricity.svg' },
+	'04.3.6': { icon: 'img/functions/wind.svg' },
+	'04.4': { icon: 'img/functions/manufactoring-construction.svg' },
+	'04.5': { icon: 'img/functions/transport.svg' },
+	'04.5.1': { icon: 'img/functions/car.svg' },
+	'04.5.2': { icon: 'img/functions/anchor.svg' },
+	'04.5.3': { icon: 'img/functions/railways.svg' },
+	'04.5.4': { icon: 'img/functions/airplane.svg' },
+	'04.6': { icon: 'img/functions/police.svg' },
+	'04.7': { icon: 'img/functions/police.svg' },
+	'04.8': { icon: 'img/functions/research.svg' },
+	'04.9': { icon: 'img/functions/police.svg' },
+	
+	'05': { icon: 'img/functions/environment.svg', color: '#2A3A03' },	
+	'05.1': { icon: 'img/functions/waste.svg' },
+	'05.2': { icon: 'img/functions/toilet.svg' },
+	'05.3': { icon: 'img/functions/pollution.svg' },
+	'05.4': { icon: 'img/functions/tree.svg' },
+	'05.6': { icon: 'img/functions/environment-admin.svg' },
+	
+	'06': { icon: 'img/functions/our-streets.svg', color: '#D33673' },		
+	'06.1': { icon: 'img/functions/housing.svg' },		
+	'06.2': { icon: 'img/functions/community.svg' },
+	'06.3': { icon: 'img/functions/water.svg' },
+	'06.4': { icon: 'img/functions/street-lights.svg' },
+	
+	'07': { icon: 'img/functions/health.svg', color: '#4E6D00' },											'07.1': { icon: 'img/functions/medical-supplies.svg' },				
+	'07.1.1': { icon: 'img/functions/medical-supplies.svg' },				
+	'07.1.2': { icon: 'img/functions/other-medical-products.svg' },				
+	'07.1.3': { icon: 'img/functions/wheelchair.svg' },		
+	'07.2': { icon: 'img/functions/health.svg' },
+	'07.2.1': { icon: 'img/functions/health.svg' },
+	'07.2.2': { icon: 'img/functions/microscope.svg' },
+	'07.2.2': { icon: 'img/functions/dental.svg' },
+	'07.3': { icon: 'img/functions/hospital.svg' },
+	'07.3.1': { icon: 'img/functions/hospital.svg' },
+	'07.3.2': { icon: 'img/functions/hospital-specialized.svg' },
+	'07.3.2': { icon: 'img/functions/dental.svg' },
+	
+	'08': { icon: 'img/functions/culture.svg', color: '#938626' },					
+	'08.2': { icon: 'img/functions/culture.svg' },					
+	'08.1': { icon: 'img/functions/sports.svg' },	
+	'08.3': { icon: 'img/functions/media.svg' },	
+	'09': { icon: 'img/functions/education.svg' },				
+	
+	'10': { icon: 'img/functions/helping-others.svg', color: '#935B3B' },
+	'10.1': { icon: 'img/functions/helping-others.svg' },
+	'10.1.2': { icon: 'img/functions/helping-others.svg' },
+	'10.2': { icon: 'img/functions/old-age.svg' },
+	'10.4': { icon: 'img/functions/family.svg' },
+	'10.7': { icon: 'img/functions/family2.svg' }
+
+};
+
